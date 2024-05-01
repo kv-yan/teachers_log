ea2935fb362779415939993fd465f953d535e53e
karVar123 <vardanyankaren924@gmail.com>
2024-05-01 21:30:11 +0400

    added splash screen

---
+++ app/src/main/java/com/varda/table/activity/splash/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.varda.table.activity.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.varda.table.R;
+import com.varda.table.activity.login.LoginActivity;
+import com.varda.table.activity.main.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent mainFlow = new Intent(SplashActivity.this, LoginActivity.class);
+            mainFlow.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(mainFlow);
+            finish();
+        }, 3000);
+    }
+}
